b1eee361b025312b3d318d6211ca8227215e44df
Barry vd. Heuvel <barryvdh@gmail.com>
2014-06-03 13:27:31 +0200

    Rename model

--- src/Models/Job.php
+++ src/Models/Job.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Eloquent\Model;
 
 /**
- * Translation model
+ * Queue jobs
  *
  * @property integer $id
  * @property integer $status
@@ -13,7 +13,12 @@ use Illuminate\Database\Eloquent\Model;
  * @property \Carbon\Carbon $created_at
  * @property \Carbon\Carbon $updated_at
  */
-class Queue extends Model{
+class Job extends Model{
+
+    const STATUS_OPEN = 0;
+    const STATUS_WAITING = 1;
+    const STATUS_STARTED = 2;
+    const STATUS_FINISHED = 3;
 
     protected $table = 'laq_async_queue';
     protected $guarded = array('id', 'created_at', 'updated_at');

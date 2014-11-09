f31102149ae18d1a97df3c2848df4a052489ca11
Graham Campbell <graham@mineuk.com>
2014-11-09 15:19:04 +0000

    Removed an unneeded constructor

--- src/Console/AsyncCommand.php
+++ src/Console/AsyncCommand.php
@@ -24,16 +24,6 @@ class AsyncCommand extends Command
     protected $description = 'Run a queue from the database';
 
     /**
-     * Create a new command instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        parent::__construct();
-    }
-
-    /**
      * Execute the console command.
      *
      * @return void

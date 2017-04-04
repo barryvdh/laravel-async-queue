47e78689457d6436bb2ac36e486102982377e536
Barry vd. Heuvel <barryvdh@gmail.com>
2017-04-04 19:51:08 +0200

    Update AsyncCommand.php

--- src/Console/AsyncCommand.php
+++ src/Console/AsyncCommand.php
@@ -4,6 +4,7 @@ namespace Barryvdh\Queue\Console;
 
 use Barryvdh\Queue\AsyncQueue;
 use Illuminate\Console\Command;
+use Illuminate\Queue\DatabaseQueue;
 use Illuminate\Queue\Worker;
 use Illuminate\Queue\WorkerOptions;
 use Symfony\Component\Console\Input\InputArgument;
@@ -31,21 +32,23 @@ class AsyncCommand extends Command
 	 * @var \Illuminate\Queue\Worker
 	 */
 	protected $worker;
+
 	/**
 	 * Create a new queue listen command.
 	 *
 	 * @param  \Illuminate\Queue\Worker  $worker
-	 * @return void
 	 */
 	public function __construct(Worker $worker)
 	{
 		parent::__construct();
+
 		$this->worker = $worker;
 	}
 
     /**
      * Execute the console command.
      *
+     * @param WorkerOptions $options
      * @return void
      */
     public function fire(WorkerOptions $options)
@@ -57,33 +60,27 @@ class AsyncCommand extends Command
 			$connection, $id, $options
 		);
     }
-    
-    
+
     /**
      *  Process the job
-     * 
+     * @param string $connectionName
+     * @param integer $id
+     * @param WorkerOptions $options
      */
     protected function processJob($connectionName, $id, $options)
     {
         $manager = $this->worker->getManager();
+
+        /** @var AsyncQueue $connection */
         $connection = $manager->connection($connectionName);
         
 		$job = $connection->getJobFromId($id);
 
-		// If we're able to pull a job off of the stack, we will process it and
-		// then immediately return back out. If there is no job on the queue
-		// we will "sleep" the worker for the specified number of seconds.
-		if ( ! is_null($job))
-		{
-            $sleep = max($job->getDatabaseJob()->available_at - time(), 0);
-            sleep($sleep);
-			return $this->worker->process(
+		if ( ! is_null($job)) {
+			$this->worker->process(
 				$manager->getName($connectionName), $job, $options
 			);
 		}
-
-		return ['job' => null, 'failed' => false];
-    
     }
 
     /**
@@ -95,20 +92,7 @@ class AsyncCommand extends Command
     {
         return array(
             array('id', InputArgument::REQUIRED, 'The Job ID'),
-            
             array('connection', InputArgument::OPTIONAL, 'The name of connection'),
-        );
-    }
-
-    /**
-     * Get the console command arguments.
-     *
-     * @return array
-     */
-    protected function getOptions()
-    {
-        return array(
-
         );
     }
 }
